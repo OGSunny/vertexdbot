c05f154092e6801d1d32be9529c8480d8fe865a1
Mikeeee <gegidzezviad05@gmail.com>
2025-06-12 17:21:46 +0400

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,6 @@
+print("Welcome to the Unordinariness Loader!")
+local player = game.Players.LocalPlayer
+if player then
+  player.Character.Humanoid.WalkSpeed = 50
+  print("Your character's speed has been increased!")
+end
